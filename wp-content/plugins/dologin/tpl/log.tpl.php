7561a15c0601ea4e369d13bbf214847fd721be5b
root <root@openlitespeed-wordpress-v5.c.gc-image-dev.internal>
2020-06-19 14:06:09 +0000

    First commit

---
+++ wp-content/plugins/dologin/tpl/log.tpl.php
@@ -0,0 +1,51 @@
+<?php
+namespace dologin;
+defined( 'WPINC' ) || exit;
+
+$__gui = GUI::get_instance();
+$__auth = Auth::get_instance();
+
+$list = $__auth->history_list( 20 );
+$count = $__auth->count_list();
+$pagination = Util::pagination( $count, 20 );
+?>
+<div class="dologin-relative">
+	<h3 class="dologin-title-short">
+		<?php echo __( 'Login Attempts Log', 'dologin' ); ?>
+	</h3>
+
+	<div class="dologin-float-submit">
+		<a href="<?php echo Util::build_url( Router::ACTION_AUTH, Auth::TYPE_CLEAR_LOG ); ?>" class="button dologin-btn-warning"><?php echo __( 'Clear records older than one month', 'dologin' ); ?></a>
+	</div>
+</div>
+
+<?php echo __( 'Total', 'dologin' ) . ': ' . $count; ?>
+
+<?php echo $pagination; ?>
+
+<table class="wp-list-table widefat striped">
+	<thead>
+	<tr>
+		<th>#</th>
+		<th><?php echo __( 'Date', 'dologin' ); ?></th>
+		<th><?php echo __( 'IP', 'dologin' ); ?></th>
+		<th><?php echo __( 'GeoLocation', 'dologin' ); ?></th>
+		<th><?php echo __( 'Login As', 'dologin' ); ?></th>
+		<th><?php echo __( 'Gateway', 'dologin' ); ?></th>
+	</tr>
+	</thead>
+	<tbody>
+	<?php foreach ( $list as $v ) : ?>
+		<tr>
+			<td><?php echo $v->id; ?></td>
+			<td><?php echo Util::readable_time( $v->dateline ); ?></td>
+			<td><?php echo $v->ip; ?></td>
+			<td><?php echo $v->ip_geo; ?></td>
+			<td><?php echo $v->username; ?></td>
+			<td><?php echo $v->gateway; ?></td>
+		</tr>
+	<?php endforeach; ?>
+	</tbody>
+</table>
+
+<?php echo $pagination; ?>
